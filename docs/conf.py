6ceffd513ec88beab1856d498971546e85d7fe00
Erica M. ("Loonatic") <l.oony@aol.com>
2023-07-07 20:39:30 -0500

    Rename project config files

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Panda3D Egg Syntax Documentation'
+copyright = '2023, Loonatic'
+author = 'Loonatic'
 
 release = '0.1'
 version = '0.1.0'
